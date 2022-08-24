6500ea537c137d37662ea59b1e6e3457fbed3d43
aorus <lmunoz-q@student.42.fr>
2022-08-24 18:32:44 +0200

    oki

--- ex00/book.py
+++ ex00/book.py
@@ -18,30 +18,51 @@ class Book:
             if not all(key in ["starter", "lunch", "dessert"] for key in recipe_list.keys()):
                 raise TypeError("error, the dictionary not contain ['starter', 'lunch', dessert] as a key value")
             elif not all(isinstance(value, Recipe) for value in recipe_list.values()):
-                raise TypeError("error, the dictionary not contain 'values' as a 'Recipe' type")
+                raise TypeError("error, the dictionary not contain 'values' as a <Recipe> type")
             self.recipe_list = recipe_list
         except TypeError as err:
             sys.exit(err)
 
     def get_recipe_by_name(self, name):
-        """Prints a recipe with the name \texttt{name} and returns the instance"""
-        for value in self.recipe_list.values():
-            if value == name:
-                print(f"{value}")
+        """Prints a recipe with the name \text{name} and returns the instance"""
+        try:
+            if not isinstance(name, str):
+                raise TypeError("error, 'name' must be a <string>")
+            for value in self.recipe_list.value():
+                if value.name == name:
+                    print(f"{name}")
+                    return value
+            raise NameError(f"error, '{name}' can't be found")
+        except TypeError as te:
+            sys.exit(te)
+        except NameError as ne:
+            sys.exit(ne)
 
     def get_recipes_by_types(self, recipe_type):
         """Get all recipe names for a given recipe_type"""
-        for key in self.recipe_list.keys():
-            if key == recipe_type:
-                print(f"{key}")
+        try:
+            if not isinstance(recipe_type, str):
+                raise TypeError("error, 'recipe_type' must be a <string>")
+            elif recipe_type not in ["starter", "lunch", "dessert"]:
+                raise TypeError("error, recipe_type must be a 'starter', 'lunch' or 'dessert'")
+            for key in self.recipe_list.keys():
+                if key == recipe_type:
+                    x = []
+                    for value in self.recipe_list[key]:
+                        x += value.name
+                    return x
+        except TypeError as te:
+            sys.exit(te)
 
     def add_recipe(self, recipe):
         """Add a recipe to the book and update last_update"""
-        if not isinstance(recipe, Recipe):
-            print("'recipe' need to be a 'Recipe' type")
-        self.recipe_list.update(recipe)
-        """update time here"""
+        try:
+            if not isinstance(recipe, Recipe):
+                raise TypeError("'recipe' need to be a <Recipe> type")
+            self.recipe_list.update(recipe)
+            self.last_update = datetime.datetime.now()
+        except TypeError as te:
+            sys.exit(te)
 
     def __str__(self):
         return f"The book is named '{self.name}'\nUpdated time {self.last_update}\nCreation time {self.creation_date}, The recipes are :{self.recipe_list}"
-
