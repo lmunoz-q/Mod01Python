ab830e15213d185fbe2ffbbea62cd2539853139d
aorus <lmunoz-q@student.42.fr>
2022-08-26 21:05:21 +0200

    k

--- ex01/game.py
+++ ex01/game.py
@@ -1,25 +1,18 @@
-import sys
-
-
 class GotCharacter:
-	def __int__(self, first_name, is_alive=True):
-		try:
-			if not isinstance(first_name, str) or first_name == "":
-				raise TypeError("error, 'first_name' must be a <non empty string>")
-			self.first_name = first_name
-			self.is_alive = is_alive
-		except TypeError as te:
-			sys.exit(te)
+	def __init__(self, first_name, is_alive=True):
+		self.first_name = first_name
+		self.is_alive = is_alive
 
 
 class Stark(GotCharacter):
+	"""A class representing the Stark family. Or when bad things happen to good people."""
 	def __init__(self, first_name=None, is_alive=True):
 		super().__init__(first_name=first_name, is_alive=is_alive)
-		self.family_name = "Stark"
-		self.house_words = "Winter is Coming"
+		self.family_name = 'Stark'
+		self.house_words = 'Winter is coming'
 
 	def print_house_words(self):
-		print(f"{self.house_words}")
+		print(self.house_words)
 
 	def die(self):
 		self.is_alive = False
